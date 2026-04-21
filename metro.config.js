53992b760d8f9c6bf81ac2e9232b8ce8f0571682
ramaguru <ultispirit@gmail.com>
2026-04-21 23:55:08 +0530

    fix: remove react-native-svg-transformer dependency from metro.config
    The SVG transformer is not needed since we use react-native-svg components
    directly (not imported SVG files). Reverting to default Expo metro config
    fixes the 'Cannot find module' error and allows npm start to run successfully.

--- metro.config.js
+++ metro.config.js
@@ -2,16 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// SVG transformer support
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
-};
-
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter((ext) => ext !== 'svg'),
-  sourceExts: [...config.resolver.sourceExts, 'svg'],
-};
-
 module.exports = config;
